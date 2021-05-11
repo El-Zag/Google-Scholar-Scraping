49336ff5d087c436a735431049e565ad6f4bbd83
Armellectx <56161210+Armellectx@users.noreply.github.com>
2021-05-11 15:29:32 +0200

    Add files via upload
    fonction qui donne une liste de synonymes

---
+++ syn.py
@@ -0,0 +1,24 @@
+import pubchempy as pcp
+
+#function which return a list of synonym for a given name of a molecule or a given pubchem number
+
+def synonyms(mol):
+
+    if type(mol)==int:
+        molecule = pcp.Compound.from_cid(mol)
+        return molecule.synonyms
+    elif type(mol)==str:
+        molecule = pcp.get_compounds(mol, 'name')
+        return(molecule[0].synonyms)
+    else: return ("Veuillez entrer un nom de molécule ou un numéro de référence pubchem svp")
+
+
+print(synonyms("rutin"))
+
+
+
+
+
+
+
+
